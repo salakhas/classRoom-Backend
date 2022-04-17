351af114e62898a39a5711b21b458ee561b1d7ed
Salakha S <ssalakha16@gmail.com>
2022-04-17 16:55:13 +0530

    again change

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
-const password = process.env.PASSWORD;
+const link = process.env.LINK;
 
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect(
-        `mongodb+srv://fullstackEval:${password}@cluster0.gplc8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-        
-    );
+    return mongoose.connect(link);
 };
 
 module.exports = connect;
