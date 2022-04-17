c41c9059a0eb87f819f1a815e6dbb311283a23ef
Salakha S <ssalakha16@gmail.com>
2022-04-17 16:25:50 +0530

    more changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 const app = express();
 
 const connect = require('./configs/db');
@@ -8,6 +9,8 @@ const userController = require('./controllers/user.controller');
 const teacherController  = require('./controllers/teacher.controller');
 const classController = require('./controllers/class.controller');
 
+
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.post('/register',register);
